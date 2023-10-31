783a39939b3dbdf3131472813d860e4e3995b50d
arturbabajanyan <arturbabajanyanv@gmail.com>
2023-10-31 17:25:38 -0400

    ItemizedReceipt is corrected

--- itemizedReceipt.js
+++ itemizedReceipt.js
@@ -5,14 +5,18 @@
 
 let tax = 6.5; //MA tax
 
-function logReceipt(burrito, price, sprite) {
-  console.log(`${burrito.descr} - $${burrito.price}`);
-  console.log(`${price.descr} - $${price.price}`);
-  console.log(`${sprite.descr} - $${sprite.price}`);
-  console.log("---------------------");
-  console.log(`Total - $${burrito.price + price.price + sprite.price}`);
-  console.log(`Tax - ${tax}%`);
-  console.log(`Grand total - $${((burrito.price + price.price + sprite.price) * (100 + tax)/100).toFixed(2)}`);
+const logReceipt = (...items) => {
+  let total = 0;
+  items.forEach((item) => {
+    total += item.price
+    console.log(item.descr + " - $" + item.price);
+  })
+
+  console.log("--------------\nTotal - $" + total);
+  let taxAmount = +((total * tax / 100).toFixed(2));
+  console.log(`--------------\nTax ${tax}% - $${taxAmount}`);
+  let grandTotal = total + taxAmount;
+  console.log("Grand total - $" + grandTotal.toFixed(2)); //if I don't use toFixed(2) grand total is 13.270000000000001
 }
 
 
@@ -20,10 +24,23 @@ function logReceipt(burrito, price, sprite) {
 logReceipt(
   { descr: 'Burrito', price: 5.99 },
   { descr: 'Chips & Salsa', price: 2.99 },
-  { descr: 'Sprite', price: 1.99 }
+  { descr: 'Sprite', price: 1.99 },
+  { descr: 'Coffee', price: 1.49 }
 );
 // should log something like:
 // Burrito - $5.99
 // Chips & Salsa - $2.99
 // Sprite - $1.99
 // Total - $10.97
+
+
+//old version
+// function logReceipt(burrito, price, sprite) {
+//   console.log(`${burrito.descr} - $${burrito.price}`);
+//   console.log(`${price.descr} - $${price.price}`);
+//   console.log(`${sprite.descr} - $${sprite.price}`);
+//   console.log("---------------------");
+//   console.log(`Total - $${burrito.price + price.price + sprite.price}`);
+//   console.log(`Tax - ${tax}%`);
+//   console.log(`Grand total - $${((burrito.price + price.price + sprite.price) * (100 + tax)/100).toFixed(2)}`);
+// }
