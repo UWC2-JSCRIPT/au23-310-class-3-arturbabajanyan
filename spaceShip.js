5a109a2deba1bdc33a3735c4d76fbb8e96539e1a
arturbabajanyan <arturbabajanyanv@gmail.com>
2023-11-04 07:40:32 -0400

    spaceship is done

--- spaceShip.js
+++ spaceShip.js
@@ -2,8 +2,22 @@
 // - should set two properties: name and topSpeed
 // - should have a method accelerate that logs to the console 
 //   `${name} moving to ${topSpeed}`
-
+class SpaceShip {
+    constructor(name, topSpeed) {
+      this.name = name;
+      this.topSpeed = topSpeed;
+    }
+    accelerate() {
+        return `${this.name} moving to ${this.topSpeed}`;
+      }
+  }
 
 
 // 2. Call the constructor with a couple ships, 
 // and call accelerate on both of them.
+
+const spaceShip1 = new SpaceShip("Galactica", '105000');
+const spaceShip2 = new SpaceShip("Destroyer", '39000');
+
+console.log (spaceShip1.accelerate());
+console.log (spaceShip2.accelerate());
